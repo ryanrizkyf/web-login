cee718152f1cfca3fc76d4a579a70b1dbfdc9d9a
Ryan <ryanrizkyf@gmail.com>
2020-12-03 22:30:41 +0700

    edit commit

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -43,31 +43,35 @@
                 ?>
 
                 <?php foreach ($subMenu as $sm) : ?>
-                    <li class="nav-item">
+                    <?php if ($title == $sm['title']) : ?>
+                        <li class="nav-item active">
+                        <?php else : ?>
+                        <li class="nav-item">
+                        <?php endif; ?>
                         <a class="nav-link" href="<?= base_url($sm['url']); ?>">
                             <i class="<?= $sm['icon']; ?>"></i>
                             <span><?= $sm['title']; ?></span></a>
-                    </li>
+                        </li>
+                    <?php endforeach; ?>
+
+                    <!-- Divider -->
+                    <hr class="sidebar-divider">
+
                 <?php endforeach; ?>
 
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('auth/logout'); ?>">
+                        <i class="fas fa-fw fa-sign-out-alt"></i>
+                        <span>Keluar</span></a>
+                </li>
+
                 <!-- Divider -->
-                <hr class="sidebar-divider">
+                <hr class="sidebar-divider d-none d-md-block">
 
-            <?php endforeach; ?>
-
-            <li class="nav-item">
-                <a class="nav-link" href="<?= base_url('auth/logout'); ?>">
-                    <i class="fas fa-fw fa-sign-out-alt"></i>
-                    <span>Keluar</span></a>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
-
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
+                <!-- Sidebar Toggler (Sidebar) -->
+                <div class="text-center d-none d-md-inline">
+                    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+                </div>
 
         </ul>
         <!-- End of Sidebar -->
